2c1a4e2d22a1eed352825467dbf503f7152a2694
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-16 14:25:03 -0300

    feat: pass props to single child using React.cloneElement with spread operator

--- ex/family.jsx
+++ ex/family.jsx
@@ -3,6 +3,9 @@ import React from "react";
 export default props => (
     <div>
         <h1>Família</h1>
-        {props.children}
+        <div>
+            { React.cloneElement(props.children, {...props}) } 
+            {/* cloneElement só funciona com um elemento child */}
+        </div>
     </div>
 )
